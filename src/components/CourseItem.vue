a4dfee46554e08754d629e4d882f567dc061a31f
fffanfannn <2295467@edu.vaniercollege.qc.ca>
2023-08-06 23:00:22 -0400

    test add-course button

--- src/components/CourseItem.vue
+++ src/components/CourseItem.vue
@@ -13,8 +13,20 @@
       <li>id: {{ course.id }}</li>
     </ul>
 
-    <button v-if="!isFull && !isAdded" @click="sendAdd">Add Course</button>
-    <button v-else-if="!isFull" @click="sendRemove">Remove Course</button>
+    <button
+      data-testid="add-course-btn"
+      v-if="!isFull && !isAdded"
+      @click="sendAdd"
+    >
+      Add Course
+    </button>
+    <button
+      data-testid="remove-course-btn"
+      v-else-if="!isFull"
+      @click="sendRemove"
+    >
+      Remove Course
+    </button>
   </div>
 </template>
 
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -13,27 +13,30 @@ describe("HelloWorld.vue", () => {
 });
 
 describe("CourseItem.vue", () => {
+  const course = {
+    name: "Vue.js",
+    description: "The Progressive JavaScript Framework",
+    hours: 50,
+    credits: 3,
+    location: "Online",
+    instructor: "John Doe",
+    id: 1,
+    enrollment: 10,
+  };
+
   it("renders default object when passed ", async () => {
     const wrapper = shallowMount(CourseItem);
     expect(wrapper.find("h2").text()).toBe("Course Name");
   });
 
-  it("renders props.msg when passed to course name", async () => {
-    const course = {
-      name: "Vue.js",
-      description: "The Progressive JavaScript Framework",
-      hours: 50,
-      credits: 3,
-      location: "Online",
-      instructor: "John Doe",
-      id: 1,
-      enrollment: 10,
-    };
+  it("renders props.course when passed ", async () => {
     const wrapper = shallowMount(CourseItem, {
       props: { course },
     });
-
     expect(wrapper.find("h2").text()).toBe("Vue.js");
+    expect(wrapper.find("p").text()).toBe(
+      "The Progressive JavaScript Framework"
+    );
     // setData not works for props
     // await wrapper.setData({
     //   course: {
@@ -58,35 +61,73 @@ describe("CourseItem.vue", () => {
     expect(wrapper.find("h2").text()).toBe("React");
   });
 
-  it("renders props.msg when passed to course discription", () => {
-    const course = {
-      name: "Vue.js",
-      description: "The Progressive JavaScript Framework",
-      hours: 50,
-      credits: 3,
-      location: "Online",
-      instructor: "John Doe",
-      id: 1,
-      enrollment: 10,
-    };
+  it("displays 'Add Course' button display, when isAdded is false(by default), ", () => {
     const wrapper = shallowMount(CourseItem, {
       props: { course },
     });
-
-    expect(wrapper.find("p").text()).toBe(
-      "The Progressive JavaScript Framework"
-    );
-  });
-
-  it("When isAdded is false(by default), Add Course button display", () => {
-    const wrapper = shallowMount(CourseItem);
     expect(wrapper.find("button").exists()).toBe(true);
     expect(wrapper.find("button").text()).toBe("Add Course");
   });
 
-  it("When click button, tringger the sendAdd function", async () => {
-    const wrapper = shallowMount(CourseItem);
+  it(" isFull is false and isAdded is false ", () => {
+    const wrapper = shallowMount(CourseItem, {
+      props: { course },
+    });
+    expect(wrapper.vm.isFull).toBe(false);
+    expect(wrapper.vm.isAdded).toBe(false);
+  });
+
+  it("when 'Add Course' button is clicked, emits 'addCourse' event with correct course id", async () => {
+    const wrapper = shallowMount(CourseItem, {
+      props: { course },
+    });
     await wrapper.find("button").trigger("click");
-    expect(wrapper.emitted().sendAdd[0]).toEqual(true);
+    expect(wrapper.emitted().addCourse).toBeTruthy;
+    expect(wrapper.emitted().addCourse[0]).toEqual([1]);
+  });
+
+  it("when 'Add Course' button is clicked, isAdded is updated to true", async () => {
+    const wrapper = shallowMount(CourseItem, {
+      props: { course },
+    });
+    await wrapper.find("button").trigger("click");
+    expect(wrapper.vm.isAdded).toBe(true);
+  });
+
+  it("when 'Add Course' button is clicked, 'Add Course' button is replaced ", async () => {
+    const wrapper = shallowMount(CourseItem, {
+      props: { course },
+    });
+    await wrapper.find('[data-testid="add-course-btn"]').trigger("click");
+    expect(wrapper.find('[data-testid="add-course-btn"]').exists()).toBe(false);
+  });
+
+  it("when 'Add Course' button is clicked, 'Add Course' button is replaced by 'Remove Course' button", async () => {
+    const wrapper = shallowMount(CourseItem, {
+      props: { course },
+    });
+    await wrapper.find('[data-testid="add-course-btn"]').trigger("click");
+    expect(wrapper.find('[data-testid="remove-course-btn"]').exists()).toBe(
+      true
+    );
+    expect(wrapper.find('[data-testid="remove-course-btn"]').text()).toBe(
+      "Remove Course"
+    );
+  });
+
+  it("when 'Remove Course' button is displayed, isFull is false ", () => {
+    const wrapper = shallowMount(CourseItem, {
+      props: { course },
+    });
+    expect(wrapper.vm.isFull).toBe(false);
+  });
+
+  it("when 'Add Course' button is clicked, emits 'addCourse' event with correct course id", async () => {
+    const wrapper = shallowMount(CourseItem, {
+      props: { course },
+    });
+    await wrapper.find("button").trigger("click");
+    expect(wrapper.emitted().addCourse).toBeTruthy;
+    expect(wrapper.emitted().addCourse[0]).toEqual([1]);
   });
 });
